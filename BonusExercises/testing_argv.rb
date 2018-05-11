fbe915b1476eb716a82298ddffdae1f3e251c79d
Mila Zangirolame <milazangirolame@gmail.com>
2018-05-10 23:29:08 -0300

    argv the ruby interpreter

---
+++ BonusExercises/testing_argv.rb
@@ -0,0 +1,6 @@
+puts "*** How Command line arguments work ***"
+puts "call ruby and the code you want (like this one) and add some arguments"
+puts "........................"
+
+puts "now copy and paste this to your terminal: argv.rb  one two three"
+
